7423f6bb4525a3a7e8a3d91cdd31507338e5a79b
Anthony Mendoza <anthony.mendoza310@gmail.com>
2020-02-13 16:18:46 -0800

    added another loop to create five divs with class of 'x' to each div with class 'y'

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //append X divs as child to Y divs
 //make X divs inline-block
 let etch = document.getElementById("etch")
-
+let y
 function init (dim) {
     let div = document.createElement("DIV")
    
@@ -12,12 +12,17 @@ function init (dim) {
     }
     let yAxis = etch.querySelectorAll("DIV")
     for (i=0;i<dim;i++){
-        yAxis[i].className = "y"}
-    let y = document.getElementsByClassName("y")
+        yAxis[i].className = "y"
+    }
+    y = document.getElementsByClassName("y")
     for(i=0;i<dim;i++){
         for(x=0;x<dim;x++){
             y[i].appendChild(div.cloneNode())
        }
     }
-    
+    for(i=0;i<dim;i++) {
+        for (x=0;x<dim;x++) {
+            y[i].querySelectorAll("DIV")[x].className = "x"
+        }
+    }
 }

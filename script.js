5596873a22bb3ac8326abc44486da800ffb0632c
Anthony Mendoza <anthony.mendoza310@gmail.com>
2020-02-14 18:51:14 -0800

    changing 'etch' id into 'board' to more accurately represent variable

--- script.js
+++ script.js
@@ -4,18 +4,18 @@
 function init (dim) {
     let area = dim * dim;
     let div = document.createElement("DIV");
-    let etch = document.getElementById("etch");
+    let board = document.getElementById("board");
     for (i=0;i<area;i++){
-        etch.appendChild(div.cloneNode());
+        board.appendChild(div.cloneNode());
     }
-    let cell = etch.querySelectorAll("DIV");
+    let cell = board.querySelectorAll("DIV");
     for (i=0;i<area;i++){
         cell[i].className = "cell";
     }
-    etch.style.display = "grid";
-    etch.style.gridTemplateColumns = divideSides(dim)
-    etch.style.width = "960px"
-    etch.style.height = "960px"
+    board.style.display = "grid";
+    board.style.gridTemplateColumns = divideSides(dim)
+    board.style.width = "960px"
+    board.style.height = "960px"
 }
 
 let fraction = ""
@@ -27,13 +27,13 @@ function divideSides (dim) {
 }
 
 function reset () {
-    let etch = document.getElementById("etch");
-    let dim = etch.childElementCount;
-    let children = document.getElementById("etch").childNodes
+    let board = document.getElementById("board");
+    let dim = board.childElementCount;
+    let children = document.getElementById("board").childNodes
     for (i=0;i<dim;i++){
-        etch.removeChild(etch.childNodes[0])
+        board.removeChild(board.childNodes[0])
     }
-    etch.style.gridTemplateColumns = ""
+    board.style.gridTemplateColumns = ""
     fraction = ""
     init(prompt("Into how many squares would you like to split the grid?", 16))
 }
